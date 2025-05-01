67c8043eec81d61e0782199779946cb401926270
shasni-md <shasnimd1@gmail.com>
2025-05-01 10:57:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ AUTO_VOICE: process.env.AUTO_VOICE || "true", // true or false
 AUTO_REPLY: process.env.AUTO_REPLY || "true", // true or false  
 IMAGE_LIMIT: process.env.IMAGE_LIMIT || "3",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tC37Q7B/20241220-122443.jpg",
-SESSION_ID: process.env.SESSION_ID || "4yUQHbhR#R9diJ9lupviN7ub-wd6yNwNosgy4RQhHa6p3Q_kwiz0",
+SESSION_ID: process.env.SESSION_ID || "UqVE3Y5R#OeeRLQM_KMTaeHGT2lLOiJ7qgWdTHw8anpPpgvOPC3Y",
 OWNER_NUMBER: process.env.SESSION_ID || "94783919841",
 PREFIX: process.env.PREFIX || ".",
 ANTI_DELETE: process.env.ANTI_DELETE || "true"
